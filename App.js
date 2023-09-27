8fc33b3de4f431219da49fd52fefa9208ff91751
Venomity <veeti.hietasalo@hotmail.fi>
2023-09-27 16:34:37 +0300

    palautus

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { Keyboard } from 'react-native';
 
 export default function App() {
+  const [currencies, setCurrencies] = useState([]);
+  const [selectedCurrency, setSelectedCurrency] = useState('');
+  const [amount, setAmount] = useState('');
+  const [convertedAmount, setConvertedAmount] = useState(null);
+  const [data, setData] = useState(null); 
+
+  useEffect(() => {
+    fetch('https://api.apilayer.com/exchangerates_data/latest', {
+      headers: {
+        'apikey': '4dLTJVLNgCr2VGQYIWg6O4sZiT4O5YQd'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        const currencyCodes = Object.keys(data.rates);
+        setCurrencies(currencyCodes);
+        setSelectedCurrency(currencyCodes[0]);
+        setData(data);  // Added this line
+      })
+      .catch(error => {
+        console.error("Virhe API-kutsussa:", error);
+      });
+  }, []);
+
+  const handleConversion = () => {
+    if (data && data.rates) {
+    const rate = data.rates[selectedCurrency];
+    const result = amount / rate;
+    setConvertedAmount(result.toFixed(2));
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Picker
+        selectedValue={selectedCurrency}
+        onValueChange={(value) => setSelectedCurrency(value)}
+      >
+        {currencies.map(currency => (
+          <Picker.Item key={currency} label={currency} value={currency} />
+        ))}
+      </Picker>
+      <TextInput 
+        value={amount}
+        onChangeText={setAmount}
+        placeholder="Enter amount"
+        keyboardType="numeric"
+        blurOnSubmit={true}
+        style={styles.input}
+      />
+      <Button title="Convert" onPress={() => { handleConversion(); Keyboard.dismiss(); }} />
+      {convertedAmount && <Text>Converted amount in EUR: {convertedAmount}</Text>}
     </View>
   );
 }
@@ -13,8 +63,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 40,
     justifyContent: 'center',
+    marginBottom: 60
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 20,
   },
 });
+
